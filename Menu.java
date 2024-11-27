30bdf5ea68314598d07e7bd12f5fee792fc3ddf0
Silas Tristoni <silas.tristoni12@gmail.com>
2024-11-26 21:52:12 -0300

    Terminando a tabela associativa

--- Menu.java
+++ Menu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MenuEvento {
+public class Menu {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
